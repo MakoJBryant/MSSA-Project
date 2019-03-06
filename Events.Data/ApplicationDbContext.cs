85a9afd7aaebc458a90e24da6eade414037ab70d
Mako Bryant <mako.j.bryant@outlook.com>
2019-03-06 10:57:01 -0800

    Added a FullName property to the ApplicationUser class. Created the Event class (in Events.Data). Created the Comment class (in Events.Data). Added comments to the Event class. Included get/set methods for Events and Comments in the ApplicationDbContext file.

--- Events.Data/ApplicationDbContext.cs
+++ Events.Data/ApplicationDbContext.cs
@@ -8,8 +8,13 @@ namespace Events.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+        // 5. Modify the ApplicationDbContext class to include the events and comments as IDbSet<T>:
+        // Events and Comments get/set methods were originally public, but they were giving me error code CS0053.
+        IDbSet<Event> Events { get; set; }
+
+        IDbSet<Comment> Comments { get; set; }
+
+        public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
 
