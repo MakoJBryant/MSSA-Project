fd46de357ae94e2bdaeb130116f3dd5364eadf4c
Mako Bryant <mako.j.bryant@outlook.com>
2019-04-08 11:06:13 -0700

    Added outline for unit tests

---
+++ Project.UnitTests/UnitTest3.cs
@@ -0,0 +1,85 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Project.UnitTests
+{
+    // Test identity and user authentication.
+    [TestClass]
+    public class UnitTestOne
+    {
+        // Verify user login token.
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        // Verify email confirmation.
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+
+        // Verify valid login parameters/errors.
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+
+        // Verify valid registration parameters/errors.
+        [TestMethod]
+        public void TestMethod4()
+        {
+        }
+    }
+
+    // Test blob storage interaction.
+    [TestClass]
+    public class UnitTestTwo
+    {
+        // Verify blob container creation.
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        // Verify blob upload.
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+
+        // Verify blob download.
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+
+        // Verify blob listing.
+        [TestMethod]
+        public void TestMethod4()
+        {
+        }
+    }
+
+    // Test video playing capabilities.
+    [TestClass]
+    public class UnitTestThree
+    {
+        // Verify that video player shows up.
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        // Verify that video actually plays.
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+
+        // Verify that correct video plays.
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+    }
+}
