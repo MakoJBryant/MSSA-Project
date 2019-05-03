4a5daeb44d9f92e52b6054d730591eddf65028d6
Mako J Bryant <mako.j.bryant@outlook.com>
2019-05-03 11:59:27 -0700

    Modal and identity fixes
     - Changing identity ViewModel format to Model-View-Controller format so that sign in works with modal.
     - Fixed issue where modal would not open again after closing it the first time.

---
+++ Project/Controllers/UserAccountController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using WebUI.ViewModels;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
+namespace WebUI.Controllers
+{
+
+    public class UserAccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<UserAccountViewModel> _logger;
+        private readonly Dictionary<int, string> _topics = new Dictionary<int, string>();
+
+        // Access the data and the user manager with dependency injection
+        public UserAccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, ILogger<UserAccountViewModel> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        // Get the current users ID
+        private string _currentUser { get { return _userManager.GetUserId(User); } }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult Index()
+        {
+            UserAccountViewModel UserModel = new UserAccountViewModel
+            {
+
+            };
+
+            return View(UserModel);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            UserAccountViewModel Avm = new UserAccountViewModel();
+
+            return View(Avm);
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([Bind("Email, Password")] UserAccountViewModel Input)
+        {
+            var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+            var result = await _userManager.CreateAsync(user, Input.Password);
+            if (result.Succeeded)
+            {
+                _logger.LogInformation("User created a new account with password.");
+                await _signInManager.PasswordSignInAsync(user, Input.Password, isPersistent: false, lockoutOnFailure: true);
+                return LocalRedirect("/Home/Index");
+            }
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return PartialView("_RegisterForm");
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login()
+        {
+            UserAccountViewModel model = new UserAccountViewModel();
+
+            // Clear the existing external cookie to ensure a clean login process
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([Bind("Email,Password,RememberMe")]UserAccountViewModel viewModel)
+        {
+            string returnUrl = Url.Content("~/");
+            var result = await _signInManager.PasswordSignInAsync(viewModel.Email, viewModel.Password, viewModel.RememberMe, lockoutOnFailure: true);
+            if (result.Succeeded)
+            {
+                _logger.LogInformation("User logged in.");
+                return LocalRedirect("/Home/Index");
+            }
+            else if (result.RequiresTwoFactor)
+            {
+                return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = viewModel.RememberMe });
+            }
+            else if (result.IsLockedOut)
+            {
+                _logger.LogWarning("User account locked out.");
+                return RedirectToPage("./Lockout");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return View();
+            }
+        }       
+    }
+}
---
+++ Project/Models/UserAccountViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+//using Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebUI.ViewModels
+{
+    public class UserAccountViewModel
+    {
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Confirm email")]
+        [Compare("Email", ErrorMessage = "The email and confirmation email do not match.")]
+        public string ConfirmEmail { get; set; }
+
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+        public bool RememberMe { get; set; }
+        public string ReturnUrl { get; set; }
+        public string ErrorMessage { get; set; }
+        public IEnumerable<IdentityError> Errors { get; set; }
+
+    }
+}
